319a82f791f719e9196df625fa9cebb8ecf725e0
Aishwarya <aishwarya923@gmail.com>
2015-08-27 17:22:43 +0530

    Removed unused code.

--- main.rb
+++ main.rb
@@ -1,14 +1,12 @@
 
 require 'rubygems'
 require 'sinatra'
-require "execjs"
 
 get "/" do
 	haml :index
 end
 
 post "/eval" do
-	# puts params["code"]
 	output = eval(params["code"])
 	haml :output, :locals => { :output => output }
 end
